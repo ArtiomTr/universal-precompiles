a4209b13803442dbf2b375483a8eb0aee9bb5c7c
sirse <hi@sirse.dev>
2025-11-05 14:21:19 +0200

    sha2: add support for SP1 precompiles
    Originally developed by leruaa in [sp1-patches] repository.
    [sp1-patches]: https://github.com/sp1-patches/RustCrypto-hashes/commit/0b1945eea7d9a776fd6e50ffd5fc51f0c5e6f155
    
    Co-authored-by: leruaa <aurelien.catinon@gmail.com>

--- sha2/src/sha256.rs
+++ sha2/src/sha256.rs
@@ -46,6 +46,9 @@ cfg_if::cfg_if! {
     } else if #[cfg(all(target_arch = "wasm32", target_feature = "simd128"))] {
         mod wasm32_simd128;
         use wasm32_simd128::compress;
+    } else if #[cfg(all(target_os = "zkvm", target_vendor = "succinct", target_arch = "riscv32"))] {
+        mod succinct;
+        use succinct::compress;
     } else {
         mod soft;
 
